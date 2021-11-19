7df6068bbe06befa5f6eef01f312ffc521ebd29f
An Nguyen <an.nguyenwasd@gmail.com>
2021-11-19 21:13:46 +0700

    chore: format

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1236,9 +1236,12 @@ require("packer").startup(
             end
         }
 
-        use {'nvim-telescope/telescope-dap.nvim', config=function ()
+        use {
+            "nvim-telescope/telescope-dap.nvim",
+            config = function()
                 require("telescope").load_extension("dap")
-        end}
+            end
+        }
         -- }}}
 
         -- {{{ Packer end
