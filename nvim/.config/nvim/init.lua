e1d2e3b8ab48f5b16cd7005e126ec79f8d2113c3
An Nguyen <an.nguyenwasd@gmail.com>
2021-11-20 18:02:27 +0700

    enable auto complete. Now <cr> will not auto select first entry

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -509,10 +509,10 @@ require("packer").startup(
 
                 cmp.setup(
                     {
-                        completion = {
-                            -- https://github.com/hrsh7th/nvim-cmp#completionautocomplete-type-cmptriggerevent
-                            autocomplete = false
-                        },
+                      experimental={
+                        ghost_text=true,
+                        native_menu=false
+                      },
                         snippet = {
                             -- REQUIRED - you must specify a snippet engine
                             expand = function(args)
@@ -568,7 +568,7 @@ require("packer").startup(
                             ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
                             ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
                             -- ["<c-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-                            ["<cr>"] = cmp.mapping.confirm({select = true}),
+                            ["<cr>"] = cmp.mapping.confirm({select = false}),
                             -- ["<cr>"] = cmp.mapping(
                             --     {
                             --         i = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace, select = true}),
