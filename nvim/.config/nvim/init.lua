4428f095cee7008fee07b08bf470d5cc258c3ba3
An Nguyen <an.nguyenwasd@gmail.com>
2021-11-24 16:28:07 +0700

    remove comment-frame

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1080,29 +1080,6 @@ require("packer").startup(
         }
 
         use {
-            "s1n7ax/nvim-comment-frame",
-            config = function()
-                require("nvim-comment-frame").setup(
-                    {
-                        -- if true, <leader>cf keymap will be disabled
-                        disable_default_keymap = false,
-                        -- adds custom keymap
-                        keymap = "<leader>cm",
-                        -- width of the comment frame
-                        frame_width = 70,
-                        -- wrap the line after 'n' characters
-                        line_wrap_len = 50,
-                        -- automatically indent the comment frame based on the line
-                        auto_indent = true,
-                        -- add comment above the current line
-                        add_comment_above = true,
-                        languages = {}
-                    }
-                )
-            end
-        }
-
-        use {
             "ThePrimeagen/harpoon",
             config = function()
                 local map = require "utils".map
