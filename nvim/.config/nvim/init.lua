a0d1017b5cf59d81598f0a24189ee7371f0dc74a
An Nguyen <an.nguyenwasd@gmail.com>
2022-01-11 23:45:53 +0700

    lsp: add nvim-lsp-ts-utils

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -279,6 +279,7 @@ require("packer").startup(
         use {
             "williamboman/nvim-lsp-installer",
             requires = {
+                {"jose-elias-alvarez/nvim-lsp-ts-utils"},
                 {"b0o/schemastore.nvim"},
                 {"neovim/nvim-lspconfig"},
                 {
@@ -361,6 +362,66 @@ require("packer").startup(
                     end
                 end
 
+                local setup_ts_utils = function(bufnr, client)
+                    local ts_utils = require("nvim-lsp-ts-utils")
+
+                    -- defaults
+                    ts_utils.setup(
+                        {
+                            debug = false,
+                            disable_commands = false,
+                            enable_import_on_completion = false,
+                            -- import all
+                            import_all_timeout = 5000, -- ms
+                            -- lower numbers = higher priority
+                            import_all_priorities = {
+                                same_file = 1, -- add to existing import statement
+                                local_files = 2, -- git files or files with relative path markers
+                                buffer_content = 3, -- loaded buffer content
+                                buffers = 4 -- loaded buffer names
+                            },
+                            import_all_scan_buffers = 100,
+                            import_all_select_source = false,
+                            -- if false will avoid organizing imports
+                            always_organize_imports = true,
+                            -- filter diagnostics
+                            filter_out_diagnostics_by_severity = {},
+                            filter_out_diagnostics_by_code = {},
+                            -- inlay hints
+                            auto_inlay_hints = true,
+                            inlay_hints_highlight = "Comment",
+                            inlay_hints_priority = 200, -- priority of the hint extmarks
+                            inlay_hints_throttle = 150, -- throttle the inlay hint request
+                            inlay_hints_format = {
+                                -- format options for individual hint kind
+                                Type = {},
+                                Parameter = {},
+                                Enum = {}
+                                -- Example format customization for `Type` kind:
+                                -- Type = {
+                                --     highlight = "Comment",
+                                --     text = function(text)
+                                --         return "->" .. text:sub(2)
+                                --     end,
+                                -- },
+                            },
+                            -- update imports on file move
+                            update_imports_on_move = false,
+                            require_confirmation_on_move = false,
+                            watch_dir = nil
+                        }
+                    )
+
+                    -- required to fix code action ranges and filter diagnostics
+                    ts_utils.setup_client(client)
+
+                    -- no default maps, so you may want to define some here
+                    local opts = {silent = true}
+                    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader><leader>oi", ":TSLspOrganize<CR>", opts)
+                    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rf", ":TSLspRenameFile<CR>", opts)
+                    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader><leader>i", ":TSLspImportAll<CR>", opts)
+                end
+
                 lsp_installer.on_server_ready(
                     function(server)
                         local on_attach = function(client, bufnr)
@@ -369,6 +430,10 @@ require("packer").startup(
                             end
                             local function buf_set_option(...)
                                 vim.api.nvim_buf_set_option(bufnr, ...)
+                            end
+
+                            if server.name == "tsserver" then
+                                setup_ts_utils(bufnr, client)
                             end
 
                             --Enable completion triggered by <c-x><c-o>
@@ -407,6 +472,7 @@ require("packer").startup(
 
                         local settings = {}
                         local capabilities = {}
+                        local init_options = {}
 
                         if server.name == "sumneko_lua" then
                             settings = {
@@ -429,8 +495,13 @@ require("packer").startup(
                             }
                         end
 
+                        if server.name == "tsserver" then
+                            init_options = require("nvim-lsp-ts-utils").init_options
+                        end
+
                         server:setup(
                             {
+                                init_options = init_options,
                                 on_attach = on_attach,
                                 settings = settings,
                                 capabilities = capabilities
