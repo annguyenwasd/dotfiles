013b8d393fe0616841e3d96990f014ac83316d1e
An Nguyen <an.nguyenwasd@gmail.com>
2022-06-24 18:39:51 +0700

    refactor: remove default configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -194,13 +194,7 @@ require("packer").startup(function(use)
         config = function()
             require("telescope").setup {
                 extensions = {
-                    fzf = {
-                        fuzzy = true, -- false will only do exact matching
-                        override_generic_sorter = true, -- override the generic sorter
-                        override_file_sorter = true, -- override the file sorter
-                        case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-                        -- the default case_mode is "smart_case"
-                    }
+                    fzf = { }
                 }
             }
 
@@ -257,7 +251,8 @@ require("packer").startup(function(use)
                 sections = {
                     lualine_a = {"mode"},
                     lualine_b = {
-                      {"branch", icon=""}, {"diff", source = diff_source},
+                        {"branch", icon = ""},
+                        {"diff", source = diff_source},
                         {"diagnostics", sources = {"nvim_diagnostic"}}
                     },
                     lualine_c = {"filename", "lsp_progress"},
