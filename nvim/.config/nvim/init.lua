b2b4e49d4041a55d4809f5f2a8cc22a46e46317c
An Nguyen <an.nguyenwasd@gmail.com>
2022-06-30 23:52:49 +0700

    adjust comment.nvim a little but. still not work on tsx fragments

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -771,44 +771,38 @@ require("packer").startup(function(use)
         "JoosepAlviste/nvim-ts-context-commentstring",
         config = function()
             require"nvim-treesitter.configs".setup {
-                context_commentstring = {enable = true}
+                context_commentstring = {
+                    enable = true,
+                    enable_autocmd = false,
+                }
             }
         end
     }
     use {
         'numToStr/Comment.nvim',
         config = function()
+
             require('Comment').setup({
-                -- NOTE: The example below is a proper integration and it is RECOMMENDED.
-                {
-                    ---@param ctx CommentCtx
-                    pre_hook = function(ctx)
-                        -- Only calculate commentstring for tsx filetypes
-                        if vim.bo.filetype == 'typescriptreact' then
-                            local U = require('Comment.utils')
+                pre_hook = function(ctx)
+                    local U = require 'Comment.utils'
 
-                            -- Determine whether to use linewise or blockwise commentstring
-                            local type =
-                                ctx.ctype == U.ctype.line and '__default' or
-                                    '__multiline'
-
-                            -- Determine the location where to calculate commentstring from
-                            local location = nil
-                            if ctx.ctype == U.ctype.block then
-                                location = require(
-                                               'ts_context_commentstring.utils').get_cursor_location()
-                            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion ==
-                                U.cmotion.V then
-                                location = require(
-                                               'ts_context_commentstring.utils').get_visual_start_location()
-                            end
-
-                            return
-                                require('ts_context_commentstring.internal').calculate_commentstring(
-                                    {key = type, location = location})
-                        end
+                    local location = nil
+                    if ctx.ctype == U.ctype.block then
+                        location =
+                            require('ts_context_commentstring.utils').get_cursor_location()
+                    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion ==
+                        U.cmotion.V then
+                        location =
+                            require('ts_context_commentstring.utils').get_visual_start_location()
                     end
-                }
+
+                    return
+                        require('ts_context_commentstring.internal').calculate_commentstring {
+                            key = ctx.ctype == U.ctype.line and '__default' or
+                                '__multiline',
+                            location = location
+                        }
+                end
             })
         end
     }
