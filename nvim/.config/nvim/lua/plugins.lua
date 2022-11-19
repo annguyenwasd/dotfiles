85f0bd6e5a3ebd1c909bc77ff3ff2d386592b6d5
An Nguyen <an.nguyenwasd@gmail.com>
2022-11-19 21:15:38 +0700

    nvim: add leetcode plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -352,6 +352,21 @@ require("packer").startup(function(use)
 
 	--}}}
 
+	-- {{{ Leetcode
+	use({
+		"ianding1/leetcode.vim",
+		config = function()
+			vim.g.leetcode_browser = "chrome"
+			vim.g.leetcode_solution_filetype = "javascript"
+
+			vim.keymap.set("n", "<leader>ll", ":LeetCodeList<cr>")
+			vim.keymap.set("n", "<leader>lt", ":LeetCodeTest<cr>")
+			vim.keymap.set("n", "<leader>ls", ":LeetCodeSubmit<cr>")
+			vim.keymap.set("n", "<leader>li", ":LeetCodeSignIn<cr>")
+		end,
+	})
+	-- }}}
+
 	-- {{{ Packer end
 
 	-- Automatically set up your configuration after cloning packer.nvim
