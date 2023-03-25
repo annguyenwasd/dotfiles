ed04b3c2a3ac124a373357a2e553316e41dad8a6
An Nguyen <an.nguyenwasd@gmail.com>
2023-03-25 18:12:31 +0700

    setup android

--- zsh/.config/zsh/personal.zsh
+++ zsh/.config/zsh/personal.zsh
@@ -21,6 +21,8 @@ export PATH="$PATH:/usr/local/opt/openssl@1.1/bin"
 export PATH="$PATH:-L/usr/local/opt/openssl@1.1/lib"
 export PATH="$PATH:-I/usr/local/opt/openssl@1.1/include"
 export PATH="$PATH:/usr/local/opt/openssl@1.1/lib/pkgconfig"
+
+export ANDROID_HOME="$HOME/Android/Sdk"
 #}}}
 
 # {{{ Alias
