c2aa1f434d6468e88d49f85ef65bdcc963c9a48c
An Nguyen <an.nguyenwasd@gmail.com>
2023-04-09 17:55:28 +0700

    nvim(tree): ignore node_modules

--- nvim/.config/nvim/lua/plugin-configs/explorer/init.lua
+++ nvim/.config/nvim/lua/plugin-configs/explorer/init.lua
@@ -6,23 +6,23 @@ return function()
 	local function collapse_all()
 		require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
 	end
-local lib = require("nvim-tree.lib")
+	local lib = require("nvim-tree.lib")
 
-local git_add = function()
-  local node = lib.get_node_at_cursor()
-  local gs = node.git_status
+	local git_add = function()
+		local node = lib.get_node_at_cursor()
+		local gs = node.git_status
 
-  -- If the file is untracked, unstaged or partially staged, we stage it
-  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
-    vim.cmd("silent !git add " .. node.absolute_path)
+		-- If the file is untracked, unstaged or partially staged, we stage it
+		if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+			vim.cmd("silent !git add " .. node.absolute_path)
 
-  -- If the file is staged, we unstage
-  elseif gs == "M " or gs == "A " then
-    vim.cmd("silent !git restore --staged " .. node.absolute_path)
-  end
+		-- If the file is staged, we unstage
+		elseif gs == "M " or gs == "A " then
+			vim.cmd("silent !git restore --staged " .. node.absolute_path)
+		end
 
-  lib.refresh_tree()
-end
+		lib.refresh_tree()
+	end
 
 	require("nvim-tree").setup({
 		live_filter = {
@@ -40,12 +40,15 @@ end
 					{ key = { "h" }, cb = tree_cb("close_node") },
 					{ key = "h", action = "close_node" },
 					{ key = "H", action = "collapse_all", action_cb = collapse_all },
-          { key = "ga", action = "git_add", action_cb = git_add },
+					{ key = "ga", action = "git_add", action_cb = git_add },
 				},
 			},
 		},
 		git = {
 			ignore = false,
+		},
+		filesystem_watchers = {
+			ignore_dirs = { "node_modules", ".git" },
 		},
 	})
 end
