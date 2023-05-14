dbdc7019286e28a41a881dc1e44321878f46bb8c
An Nguyen <an.nguyenwasd@gmail.com>
2023-05-14 21:42:56 +0800

    arch: add setup packages

---
+++ arch/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+yes|sudo pacman -S i3 zsh xorg dmenu alacritty firefox python node npm lazygit fzf ripgrep
+
+zsh
+chsh
+sz
+
+yes|aur nerd-fonts-meta
+yes|aur google chrome
+
+# python dependency for neovim
+python -m ensurepip --upgradepython -m ensurepip --upgrade
+pip3 install neovim
