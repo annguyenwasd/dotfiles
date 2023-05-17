75562ce7c24ae9d28c76b9abfbf7213b43149458
An Nguyen <an.nguyenwasd@gmail.com>
2023-05-17 19:18:44 +0700

    arch: add pulseaudio

--- arch/install.sh
+++ arch/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 su
-yes|sudo pacman -S i3 tmux git neovim vim stow zsh xorg dmenu alacritty firefox python node npm lazygit fzf ripgrep openssh xclip curl unzip feh java-runtime-common java-environment-common jre-openjdk jdk-openjdk openjdk-doc openjdk-src os-prober polkit sudo vi xdg-user-dirs
+yes|sudo pacman -S i3 tmux git neovim vim stow zsh xorg dmenu alacritty firefox python node npm lazygit fzf ripgrep openssh xclip curl unzip feh java-runtime-common java-environment-common jre-openjdk jdk-openjdk openjdk-doc openjdk-src os-prober polkit sudo vi xdg-user-dirs pulseaudio pulsemixer
 
 zsh
 chsh
