bf0459f2d4593526fe5e6b27c0c5754fe88c6dcc
An Nguyen <an.nguyenwasd@gmail.com>
2024-06-12 09:43:51 +0700

    git: add git_worktree plugin

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -200,4 +200,27 @@ return {
 			})
 		end,
 	},
+	{
+		"ThePrimeagen/git-worktree.nvim",
+		config = function()
+			require("git-worktree").setup({})
+			require("telescope").load_extension("git_worktree")
+		end,
+		keys = {
+			{
+				"<leader>wl",
+				function()
+					require("telescope").extensions.git_worktree.git_worktrees()
+				end,
+				desc("git: worktree checkout"),
+			},
+			{
+				"<leader>wj",
+				function()
+					require("telescope").extensions.git_worktree.create_git_worktree()
+				end,
+				desc("git: create git worktree"),
+			},
+		},
+	},
 }
