1e5fb6d69299eb56097b37215310c11c45aabbb0
An Nguyen <an.nguyenwasd@gmail.com>
2024-08-16 23:35:41 +0700

    nvim(leetcode): add keybinds

--- nvim/.config/nvim/lua/plugins/leetcode.lua
+++ nvim/.config/nvim/lua/plugins/leetcode.lua
@@ -7,4 +7,11 @@ return {
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 	},
+	keys = {
+		{ "<leader>ll", "<cmd>Leet list<cr>", desc = desc("leetcode: list") },
+		{ "<leader>lt", "<cmd>Leet test<cr>", desc = desc("leetcode: test") },
+		{ "<leader>lc", "<cmd>Leet console<cr>", desc = desc("leetcode: console") },
+		{ "<leader>lo", "<cmd>Leet open<cr>", desc = desc("leetcode: open") },
+		{ "<leader>ly", "<cmd>Leet yark<cr>", desc = desc("leetcode: yark") },
+	},
 }
