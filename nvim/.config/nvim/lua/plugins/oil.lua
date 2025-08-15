741e9b1fc1bac3e8eb0384fab40e4ad86951dc7b
An Nguyen <an.nguyenwasd@gmail.com>
2025-08-15 23:19:46 +0700

    nvim: disabled Oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,9 @@
 return {
 	"stevearc/oil.nvim",
 	cmd = "Oil",
+	enabled = false,
 	opts = {
-      skip_confirm_for_simple_edits = true,
+		skip_confirm_for_simple_edits = true,
 		-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
 		-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
 		-- Additionally, if it is a string that matches "actions.<name>",
