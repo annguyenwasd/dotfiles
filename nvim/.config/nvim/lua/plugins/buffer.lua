c740a691f013766ecd9c06c12514c397c28efc1d
An Nguyen <an.nguyenwasd@gmail.com>
2025-08-18 18:32:19 +0700

    nvim(buffer): make sure fzf is installed correctly

--- nvim/.config/nvim/lua/plugins/buffer.lua
+++ nvim/.config/nvim/lua/plugins/buffer.lua
@@ -1,38 +1,42 @@
 return {
-	{
-		"kevinhwang91/nvim-bqf",
-		ft = "qf",
-		dependencies = {
-			{
-				"junegunn/fzf.vim",
-				dir = "~/.fzf",
-				build = "./install --bin",
-			},
-		},
-		config = function()
-			require("bqf").setup({
-				preview = {
-					auto_preview = true,
-					winblend = 0,
-					win_height = 999,
-				},
-			})
-		end,
-	},
-	{
-		"numtostr/BufOnly.nvim",
-		init = function()
-			vim.g.bufonly_delete_non_modifiable = true
-		end,
-		keys = {
-			{ "<leader>bo", ":BufOnly<CR>", desc = desc("buf: buf only") },
-		},
-	},
-	{
-		"yorickpeterse/nvim-window",
-		config = true,
-		keys = {
-			{ "<leader>ww", "<cmd>lua require('nvim-window').pick()<cr>", desc = desc("buf: Picking neovim's window") },
-		},
-	},
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf",
+    dependencies = {
+      -- in your plugins spec
+      {
+        "junegunn/fzf",
+        build = "./install --all",
+      },
+      {
+        "junegunn/fzf.vim",
+        dependencies = { "junegunn/fzf" },
+      }
+    },
+    config = function()
+      require("bqf").setup({
+        preview = {
+          auto_preview = true,
+          winblend = 0,
+          win_height = 999,
+        },
+      })
+    end,
+  },
+  {
+    "numtostr/BufOnly.nvim",
+    init = function()
+      vim.g.bufonly_delete_non_modifiable = true
+    end,
+    keys = {
+      { "<leader>bo", ":BufOnly<CR>", desc = desc("buf: buf only") },
+    },
+  },
+  {
+    "yorickpeterse/nvim-window",
+    config = true,
+    keys = {
+      { "<leader>ww", "<cmd>lua require('nvim-window').pick()<cr>", desc = desc("buf: Picking neovim's window") },
+    },
+  },
 }
