47e44e5eedb1e7a13bc5739380ad677bd5ac2a48
An Nguyen <an.nguyenwasd@gmail.com>
2025-08-18 18:09:40 +0700

    nvim(telescope): add flag to work with cmake

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -175,10 +175,10 @@ return {
 			},
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				enabled = function()
+				cond = function()
 					return not is_work_profile()
 				end,
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				build = "cmake -S. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 				config = function()
 					require("telescope").load_extension("fzf")
 				end,
