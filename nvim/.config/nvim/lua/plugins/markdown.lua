cd0359ae1fc07d64bad4709620a597760e5094e7
An Nguyen <an.nguyenwasd@gmail.com>
2025-08-21 22:02:00 +0700

    nvim: disable markview

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,26 +1,26 @@
 return {
-	{
-		"OXY2DEV/markview.nvim",
-		ft = "markdown",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-		keys = {
-			{
-				"<leader>mv",
-				"<cmd>Markview<cr>",
-				desc = desc("markdown: Toggle markview"),
-			},
-		},
-	},
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-	},
+  {
+    "OXY2DEV/markview.nvim",
+    cond = function() return false end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      {
+        "<leader>mv",
+        "<cmd>Markview<cr>",
+        desc = desc("markdown: Toggle markview"),
+      },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+  },
 }
