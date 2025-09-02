a739d7e71e9811c29e7bdbc9d9524bde85e31385
An Nguyen <an.nguyenwasd@gmail.com>
2025-09-02 19:01:03 +0700

    nvim(leetcode): make single file can have lsp - ts_ls

--- nvim/.config/nvim/lua/utils/lsp.lua
+++ nvim/.config/nvim/lua/utils/lsp.lua
@@ -119,6 +119,19 @@ local get_ts_ls_config = function()
     capabilities = capabilities,
     settings = settings,
     handlers = handlers,
+    root_dir = function(bufnr, on_dir)
+      -- The project root is where the LSP can be started from
+      -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+      -- We select then from the project root, which is identified by the presence of a package
+      -- manager lock file.
+      local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+      -- Give the root markers equal priority by wrapping them in a table
+      root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers } or root_markers
+      local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":h")
+
+
+      on_dir(project_root)
+    end,
   }
 end
 
