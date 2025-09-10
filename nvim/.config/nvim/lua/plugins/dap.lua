7ae083ee366b84a45bfa9455abb6bfae768de39c
An Nguyen <an.nguyenwasd@gmail.com>
2025-09-10 23:58:26 +0700

    nvim(dap): add hover and preview keybinds

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -3,68 +3,26 @@ return {
   {
     "mfussenegger/nvim-dap",
     init = function()
-      vim.cmd([[
-        au FileType dap-repl lua require('dap.ext.autocompl').attach()
-      ]])
+      vim.cmd([[ au FileType dap-repl lua require('dap.ext.autocompl').attach() ]])
     end,
     keys = {
-      {
-        "<localleader>bb",
-        "<cmd>lua require('dap').toggle_breakpoint()<cr>",
-        desc = desc("debug: toggle breakpoint"),
-      },
-      {
-        "<localleader>bc",
-        function()
-          require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-        end,
-        desc = desc("debug: toggle breakpoint with a condition"),
-      },
-      {
-        "<localleader>bl",
-        function()
-          require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-        end,
-        desc = desc("debug: log point message"),
-      },
-      {
-        "<localleader>BB",
-        function()
-          require("dap").list_breakpoints()
-        end,
-        desc = desc("debug: list breakpoints"),
-      },
-      {
-        "<localleader>c",
-        "<cmd>lua require('dap').continue()<cr>",
-        desc = desc("debug: continue/start debugging"),
-      },
-      { "`o",              "<cmd>lua require('dap').step_over()<cr>",     desc = desc("debug: step over") },
-      { "`i",              "<cmd>lua require('dap').step_into()<cr>",     desc = desc("debug: step into") },
-      { "`u",              "<cmd>lua require('dap').step_out()<cr>",      desc = desc("debug: step out") },
-      { "`j",              "<cmd>lua require('dap').down()<cr>",          desc = desc("debug: Go down in current stacktrace without stepping.") },
-      { "`k",              "<cmd>lua require('dap').up()<cr>",            desc = desc("debug: Go up in current stacktrace without stepping.") },
-      { "<localleader>dt", "<cmd>lua require('dap').terminate({all=true})<cr>",     desc = desc("debug: terminate") },
+      { "<localleader>bb", "<cmd>lua require('dap').toggle_breakpoint()<cr>", desc = desc("debug: toggle breakpoint") },
+      { "<localleader>bc", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = desc("debug: toggle breakpoint with a condition") },
+      { "<localleader>bl", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, desc = desc("debug: log point message") },
+      { "<localleader>BB", function() require("dap").list_breakpoints() end, desc = desc("debug: list breakpoints") },
+      { "<localleader>c", "<cmd>lua require('dap').continue()<cr>", desc = desc("debug: continue/start debugging") },
+      { "`o","<cmd>lua require('dap').step_over()<cr>", desc = desc("debug: step over") },
+      { "`hk","<cmd>lua require('dap.ui.widgets').hover()<cr>", mode = {"n", "v"} ,desc = desc("debug: hover, open float at cursor") },
+      { "`hh","<cmd>lua require('dap.ui.widgets').preview()<cr>", mode = {"n", "v"} ,desc = desc("debug: preview, open a preview window") },
+      { "`i","<cmd>lua require('dap').step_into()<cr>", desc = desc("debug: step into") },
+      { "`u","<cmd>lua require('dap').step_out()<cr>", desc = desc("debug: step out") },
+      { "`j","<cmd>lua require('dap').down()<cr>", desc = desc("debug: Go down in current stacktrace without stepping.") },
+      { "`k","<cmd>lua require('dap').up()<cr>", desc = desc("debug: Go up in current stacktrace without stepping.") },
+      { "<localleader>dt", "<cmd>lua require('dap').terminate({all=true})<cr>",     desc = desc("debug: terminate")},
       { "<localleader>dr", "<cmd>lua require('dap').repl.toggle()<cr>",   desc = desc("debug: open repl") },
       { "<localleader>dc", "<cmd>lua require('dap').run_to_cursor()<cr>", desc = desc("debug: run to cursor") },
-      {
-        "<localleader>ds",
-        function()
-          local widgets = require("dap.ui.widgets")
-          local my_sidebar = widgets.sidebar(widgets.scopes)
-          my_sidebar.open()
-        end,
-        desc = desc("debug: widge shows scopes"),
-      },
-      {
-        "<localleader>df",
-        function()
-          local widgets = require("dap.ui.widgets")
-          local my_sidebar = widgets.sidebar(widgets.frames)
-          my_sidebar.open()
-        end,
-        desc = desc("debug: widget shows frames"),
-      },
+      { "<localleader>ds", function() local widgets = require("dap.ui.widgets") local my_sidebar = widgets.sidebar(widgets.scopes) my_sidebar.open() end, desc = desc("debug: widge shows scopes") },
+      { "<localleader>df", function() local widgets = require("dap.ui.widgets") local my_sidebar = widgets.sidebar(widgets.frames) my_sidebar.open() end, desc = desc("debug: widget shows frames") },
     },
   },
   {
